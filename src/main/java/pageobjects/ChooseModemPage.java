05ef123e76e40f7aa0eae4e658d13cffeb6c7a85
Abraham Ora <abrahamora@SP-AOra-MacbookPro2019.local>
2021-03-23 14:59:15 +0800

    Refartor ChooseModem

--- src/main/java/pageobjects/ChooseModemPage.java
+++ src/main/java/pageobjects/ChooseModemPage.java
@@ -7,7 +7,13 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import io.qameta.allure.Step;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import utilities.ActionUtilities;
+import utilities.ConfigUtilities;
 
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class ChooseModemPage extends BasePage {
@@ -17,77 +23,151 @@ public class ChooseModemPage extends BasePage {
     }
 
 
+
+    //    ================ REFACTORED SECTION =====================
+
+    @Step("Step: Policy Checkbox")
+    public void clickCheckBox() throws IOException {
+        log.info("Step: Click Policy Checkbox");
+        action.takeSnapShot("User clicked the checkbox");
+        action.click("policyCheckbox");
+    }
+
+    @Step("Step: Verify if user is in Get Started Page")
+    public boolean verifyIfGetStartedPage() throws Exception {
+        log.info("Step: Verify if user is in Get Started Page");
+        Assert.assertTrue(action.waitForElementToBeVisible("btnGetStarted", ConfigUtilities.Timers.slow));
+        action.takeSnapShot("User is in Get Started Page");
+        return action.isDisplayed("btnGetStarted");
+    }
+
+    @Step("Step: Verify Get Started Pages")
+    public void verifyChooseModemOne() throws Exception {
+        log.info("Step: Verify Get Started Pages");
+        verifyIfGetStartedPage();
+        assertEquals(excel.getTestdata("text1"), action.getText("txtGetStartedTitle"));
+        action.takeSnapShot(excel.getTestdata("text1"));
+        verifyIfGetStartedPage();
+        action.click("txtGetStartedTitle");
+        action.sendKeys("txtMobileNumber",excel.getTestdata("mobileNumber"));
+    }
+
+    //   ================= END OF REFACTORED SECTION =============
+
     // This object is displayed after entering your nickname
 
     /*
       TERMS AND AGREEMENT CHECKBOX SECTION
      */
-    @AndroidFindBy(xpath = "//android.widget.CheckBox[@resource-id='ph.com.globe.mybusiness:id/cb_kyc_consent_promo_updates']")
-    @iOSXCUITFindBy( id = "a" )
-    private MobileElement policyCheckbox;
-
-    @Step("Step: Policy Checkbox")
-    public void clickCheckBox(){
-        log.info("Step: Click Policy Checkbox");
-        action.click(policyCheckbox);
-    }
+//    @AndroidFindBy(xpath = "//android.widget.CheckBox[@resource-id='ph.com.globe.mybusiness:id/cb_kyc_consent_promo_updates']")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement policyCheckbox;
+//
+//    @Step("Step: Policy Checkbox")
+//    public void clickCheckBox() {
+//        log.info("Step: Click Policy Checkbox");
+//        action.click(policyCheckbox);
+//    }
 
     /*
     This is for Let's Go button in Term Agreement
      */
 
-    @AndroidFindBy(xpath = "//android.widget.Button[@resource-id='ph.com.globe.mybusiness:id/btn_next']")
-    @iOSXCUITFindBy( id = "a" )
-    private MobileElement letsGoBtn;
-
-    @Step("Step: Policy Let's Go Button")
-    public void clickLetsGoBtn(){
-        log.info("Step: Step: Click Policy Let's Go Button");
-        action.click(letsGoBtn);
-    }
+//    @AndroidFindBy(xpath = "//android.widget.Button[@resource-id='ph.com.globe.mybusiness:id/btn_next']")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement letsGoBtn;
+//
+//    @Step("Step: Policy Let's Go Button")
+//    public void clickLetsGoBtn() {
+//        log.info("Step: Step: Click Policy Let's Go Button");
+//        action.click(letsGoBtn);
+//    }
 
     /*
     This is for Skip link button
      */
 
-    @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id='ph.com.globe.mybusiness:id/tv_skip']")
-    @iOSXCUITFindBy( id = "a" )
-    private MobileElement skipBtn;
-
-    @Step("Step: Skip Button in Profile Details")
-    public void clickSkipBtn(){
-        log.info("Step: Click Skip Button in Profile Details");
-        action.click(skipBtn);
-    }
+//    @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id='ph.com.globe.mybusiness:id/tv_skip']")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement skipBtn;
+//
+//    @Step("Step: Skip Button in Profile Details")
+//    public void clickSkipBtn() {
+//        log.info("Step: Click Skip Button in Profile Details");
+//        action.click(skipBtn);
+//    }
 
     /*
     This is for Modal Close Button
      */
 
-    @AndroidFindBy(xpath = "//android.widget.RelativeLayout[@resource-id='ph.com.globe.mybusiness:id/rl_dialog_button_container']")
-    @iOSXCUITFindBy( id = "a" )
-    private MobileElement closeModalBtn;
-
-    @Step("Step: Close Modal Button")
-    public void clickCloseModalBtn(){
-        log.info("Step: Click Close Modal Button");
-        action.click(closeModalBtn);
-    }
-
+//    @AndroidFindBy(xpath = "//android.widget.RelativeLayout[@resource-id='ph.com.globe.mybusiness:id/rl_dialog_button_container']")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement closeModalBtn;
+//
+//    @Step("Step: Close Modal Button")
+//    public void clickCloseModalBtn() {
+//        log.info("Step: Click Close Modal Button");
+//        action.click(closeModalBtn);
+//    }
 
     /*
     This is for the First Modem Selection
      */
 
-    @AndroidFindBy(xpath = "//android.widget.ImageView[@resource-id='ph.com.globe.mybusiness:id/iv_modem']")
-    @iOSXCUITFindBy( id = "a" )
-    private MobileElement modemOneBtn;
+//    @AndroidFindBy(xpath = "(//android.widget.ImageView)[1]")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement modemOneBtn;
+//
+//    @Step("Step: Modem1 ")
+//    public void clickModemOneBtn() {
+//        log.info("Step: Click Modem 1");
+//        action.click(modemOneBtn);
+//    }
 
-    @Step("Step: Modem1 ")
-    public void clickModemOneBtn(){
-        log.info("Step: Click Modem 1");
-        action.click(modemOneBtn);
-    }
+    /*
+    This is for the Second Modem Selection
+     */
+
+//    @AndroidFindBy(xpath = "//android.widget.ImageView)[2]")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement modemTwoBtn;
+//
+//    @Step("Step: Modem2 ")
+//    public void clickModemTwoBtn() {
+//        log.info("Step: Click Modem 2");
+//        action.click(modemTwoBtn);
+//    }
+
+    /*
+    This is for the Third Modem Selection
+     */
+
+//    @AndroidFindBy(xpath = "//android.widget.ImageView)[3]")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement modemThreeBtn;
+//
+//    @Step("Step: Modem 3 ")
+//    public void clickModemThreeBtn() {
+//        log.info("Step: Click Modem 3");
+//        action.click(modemThreeBtn);
+//    }
+
+    /*
+    This is for the Third Modem Selection
+     */
+
+//    @AndroidFindBy(xpath = "//android.widget.ImageView)[4]")
+//    @iOSXCUITFindBy(id = "a")
+//    private MobileElement modemFourBtn;
+//
+//    @Step("Step: Modem 4 ")
+//    public void clickModemFourBtn() {
+//        log.info("Step: Click Modem 4");
+//        action.click(modemFourBtn);
+//    }
+
+
 
 
 }
--- src/test/java/testcases/VerifyChooseModem.java
+++ src/test/java/testcases/VerifyChooseModem.java
@@ -10,40 +10,150 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 @Listeners(testcases.ListenerTestcase.class)
-public class VerifyChooseModem extends BaseTestcase{
+public class VerifyChooseModem extends BaseTestcase {
 
     @Test(testName = "VerifyChooseModem1", priority = 2)
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Description: User will check for the first modem")
-    @Story("Story: Choose modem 1")
-    public void verifyChooseModem() throws Exception {
-
-        assertTrue(getStartedPage.verifyIfGetStartedPage());
-        getStartedPage.clickBtnGetStarted();
-        welcomePage.enterPrepaidNumber("09271080510");
-        welcomePage.clickBtnNext();
-
-
-        assertTrue(secureAppPage.verifyIfSecurePage());
-        secureAppPage.enterPin("1111");
-        secureAppPage.verifyIfBtnNextIsEnabled();
-        secureAppPage.clickBtnNext();
-
-        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
-        secureAppPage.enterPin("1111");
-        secureAppPage.verifyIfBtnNextIsEnabled();
-        secureAppPage.clickBtnNext();
-
-        customerProfilePage.enterNickname("Test Abe");
-        customerProfilePage.clickBtnNext();
-
-        chooseModemPage.clickCheckBox();
-        chooseModemPage.clickLetsGoBtn();
-
-        chooseModemPage.clickSkipBtn();
-        chooseModemPage.clickCloseModalBtn();
-        chooseModemPage.clickModemOneBtn();
-
+    @Story("Story: Choose modem ")
+    public void verifyChooseModemOne() throws Exception {
+        chooseModemPage.verifyChooseModemOne();
     }
+
+//    @Test(testName = "VerifyChooseModem1", priority = 2)
+//    @Severity(SeverityLevel.NORMAL)
+//    @Description("Test Description: User will check for the first modem")
+//    @Story("Story: Choose modem 1")
+//    public void verifyChooseModemOne() throws Exception {
+//
+//        assertTrue(getStartedPage.verifyIfGetStartedPage());
+//        getStartedPage.clickBtnGetStarted();
+//        welcomePage.enterPrepaidNumber("09271080510");
+//        welcomePage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePage());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        customerProfilePage.enterNickname("Abe");
+//        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
+//        customerProfilePage.clickBtnNext();
+//
+//        chooseModemPage.clickCheckBox();
+//        chooseModemPage.clickLetsGoBtn();
+//
+//        chooseModemPage.clickSkipBtn();
+//        chooseModemPage.clickCloseModalBtn();
+//        chooseModemPage.clickModemOneBtn();
+//
+//    }
+//
+//    @Test(testName = "VerifyChooseModem2", priority = 2)
+//    @Severity(SeverityLevel.NORMAL)
+//    @Description("Test Description: User will check for the second modem")
+//    @Story("Story: Choose modem 2")
+//    public void verifyChooseModemTwo() throws Exception {
+//
+//        assertTrue(getStartedPage.verifyIfGetStartedPage());
+//        getStartedPage.clickBtnGetStarted();
+//        welcomePage.enterPrepaidNumber("09271080510");
+//        welcomePage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePage());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        customerProfilePage.enterNickname("Abe");
+//        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
+//        customerProfilePage.clickBtnNext();
+//
+//        chooseModemPage.clickCheckBox();
+//        chooseModemPage.clickLetsGoBtn();
+//
+//        chooseModemPage.clickSkipBtn();
+//        chooseModemPage.clickCloseModalBtn();
+//        chooseModemPage.clickModemTwoBtn();
+//
+//    }
+//
+//    @Test(testName = "VerifyChooseModem3", priority = 2)
+//    @Severity(SeverityLevel.NORMAL)
+//    @Description("Test Description: User will check for the third modem")
+//    @Story("Story: Choose modem 3")
+//    public void verifyChooseModemThree() throws Exception {
+//
+//        assertTrue(getStartedPage.verifyIfGetStartedPage());
+//        getStartedPage.clickBtnGetStarted();
+//        welcomePage.enterPrepaidNumber("09271080510");
+//        welcomePage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePage());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        customerProfilePage.enterNickname("Abe");
+//        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
+//        customerProfilePage.clickBtnNext();
+//
+//        chooseModemPage.clickCheckBox();
+//        chooseModemPage.clickLetsGoBtn();
+//
+//        chooseModemPage.clickSkipBtn();
+//        chooseModemPage.clickCloseModalBtn();
+//        chooseModemPage.clickModemThreeBtn();
+//
+//    }
+//
+//    @Test(testName = "VerifyChooseModem4", priority = 2)
+//    @Severity(SeverityLevel.NORMAL)
+//    @Description("Test Description: User will check for the fourth modem")
+//    @Story("Story: Choose modem 4")
+//    public void verifyChooseModemFour() throws Exception {
+//
+//        assertTrue(getStartedPage.verifyIfGetStartedPage());
+//        getStartedPage.clickBtnGetStarted();
+//        welcomePage.enterPrepaidNumber("09271080510");
+//        welcomePage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePage());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
+//        secureAppPage.enterPin("1111");
+//        secureAppPage.verifyIfBtnNextIsEnabled();
+//        secureAppPage.clickBtnNext();
+//
+//        customerProfilePage.enterNickname("Abe");
+//        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
+//        customerProfilePage.clickBtnNext();
+//
+//        chooseModemPage.clickCheckBox();
+//        chooseModemPage.clickLetsGoBtn();
+//
+//        chooseModemPage.clickSkipBtn();
+//        chooseModemPage.clickCloseModalBtn();
+//        chooseModemPage.clickModemFourBtn();
+//
+//    }
 
 }
