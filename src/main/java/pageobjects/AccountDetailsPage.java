acd4a7a8321a246b67d9efd30de5d50420c6e74b
EybTheGreat <abraham.morial.ora@gmail.com>
2021-05-14 16:45:20 +0800

    Added Account Details Page Test

---
+++ src/main/java/pageobjects/AccountDetailsPage.java
@@ -0,0 +1,201 @@
+package pageobjects;
+
+import io.appium.java_client.MobileDriver;
+import io.qameta.allure.Step;
+import org.testng.Assert;
+import utilities.ActionUtilities;
+import utilities.ConfigUtilities;
+
+public class AccountDetailsPage extends BasePage{
+
+//*********  Scenarios  **************//
+    //    Verify Account Details Page
+    //    Edit Nickname
+    //    Verify Phone number and status
+    //    Verify Profile status
+    //    Go to my profile
+    //    Upload photo
+
+    MobileDriver driver;
+
+    public AccountDetailsPage(MobileDriver driver) {
+        this.driver = driver;
+    }
+
+
+    //*********  PageObject  **************//
+
+
+    // For Account Details verification
+    @Step("Step: Verify if Account Details Link is Enabled")
+    public Boolean verifyLinkAccountDetailsIsEnabled() throws Exception {
+        log.info("Step: Verify if Account Details Link is Enabled");
+        Assert.assertTrue(action.waitForElementToBeVisible("lnkAccountDetails", ConfigUtilities.Timers.slow));
+        return action.isEnabled("lnkAccountDetails");
+    }
+
+    @Step("Step: Account Details Link")
+    public void clickLinkAccountDetails() throws Exception {
+        log.info("Step: Click the Account Details Link");
+        action.takeSnapShot("User clicked the Account Details Link");
+        action.waitForElementToBeClickable("lnkAccountDetails",ConfigUtilities.Timers.slow);
+        action.click("lnkAccountDetails");
+    }
+
+    // For Nickname verification
+    @Step("Step: Verify if Nickname in Account Details section Displayed")
+    public Boolean verifyNicknameAccountDetailsIsDisplayed() throws Exception {
+        log.info("Step: Verify if Nickname in Account Details section is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("lblNickname", ConfigUtilities.Timers.slow));
+        Assert.assertEquals(excel.getTestdata("nickname"),action.getText("lblNickname"));
+        return action.isDisplayed("lblNickname");
+    }
+
+    // Account Details text
+    @Step("Step: Verify if Nickname in Account Details section Displayed")
+    public Boolean verifyAccountDetailsTxtIsDisplayed() throws Exception {
+        log.info("Step: Verify if Account Details text is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("txtTitle", ConfigUtilities.Timers.slow));
+        Assert.assertEquals(excel.getTestdata("text1"),action.getText("txtTitle"));
+        return action.isDisplayed("txtTitle");
+    }
+
+    // For Camera Icon verification
+    @Step("Step: Verify if Camera Icon is Enabled")
+    public Boolean verifyCameraIconIsEnable() throws Exception {
+        log.info("Step: Verify if Camera Icon is Enabled");
+        Assert.assertTrue(action.waitForElementToBeVisible("iconCamera", ConfigUtilities.Timers.slow));
+        return action.isEnabled("iconCamera");
+    }
+
+    // For Image verification
+    @Step("Step: Verify if Image Icon is Displayed")
+    public Boolean verifyAccountImageIsDisplayed() throws Exception {
+        log.info("Step: Verify if Image Icon is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("accntImg", ConfigUtilities.Timers.slow));
+        return action.isDisplayed("accntImg");
+    }
+
+    // For Hamburger Button verification
+    @Step("Step: Verify if Hamburger Button is Enabled")
+    public Boolean verifyHamburgerBtnIsEnabled() throws Exception {
+        log.info("Step: Verify if Hamburger Button is Enabled");
+        Assert.assertTrue(action.waitForElementToBeVisible("btnHamburgerAP", ConfigUtilities.Timers.slow));
+        return action.isEnabled("btnHamburgerAP");
+    }
+
+    // For Edit Nickname Link verification
+    @Step("Step: Verify if Edit Nickname Link is Enabled")
+    public Boolean verifyEditNicknameLinkIsEnabled() throws Exception {
+        log.info("Step: Verify if Edit Nickname Link is Enabled");
+        Assert.assertTrue(action.waitForElementToBeVisible("editNicknameLink", ConfigUtilities.Timers.slow));
+        return action.isEnabled("editNicknameLink");
+    }
+
+    @Step("Step: Click edit nickname")
+    public void clickEditNickname() throws Exception {
+        log.info("Step: Click edit nickname");
+        action.takeSnapShot("User clicked edit nickname");
+        action.waitForElementToBeClickable("editNicknameLink",ConfigUtilities.Timers.slow);
+        action.click("editNicknameLink");
+    }
+
+    // For Prepaid Number Label verification
+    @Step("Step: Verify if Prepaid Number Label is Displayed")
+    public Boolean verifyLblPrepaidNumberIsDisplayed() throws Exception {
+        log.info("Step: Verify if Prepaid Number Label is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("lblPrepaidInternetNumber", ConfigUtilities.Timers.slow));
+        Assert.assertEquals(excel.getTestdata("lblPrepaidNumber"),action.getText("lblPrepaidInternetNumber"));
+        return action.isDisplayed("lblPrepaidInternetNumber");
+    }
+
+    // For Prepaid Number verification
+    @Step("Step: Verify if Prepaid Number is Displayed")
+    public Boolean verifyPrepaidNumberIsDisplayed() throws Exception {
+        log.info("Step: Verify if Prepaid Number is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("prepaidNumber", ConfigUtilities.Timers.slow));
+        Assert.assertEquals(excel.getTestdata("prepNumber"),action.getText("prepaidNumber"));
+        return action.isDisplayed("prepaidNumber");
+    }
+
+    // For Edit Prepaid Number Status verification
+    @Step("Step: Verify if Prepaid Number Status is Displayed")
+    public Boolean verifyPrepaidNumberStatusIsDisplayed() throws Exception {
+        log.info("Step: Verify if Prepaid Number Status is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("prepaidNumberStatus", ConfigUtilities.Timers.slow));
+        return action.isDisplayed("prepaidNumberStatus");
+    }
+
+    // For Edit My Profile Label verification
+    @Step("Step: Verify if My Profile Label is Displayed")
+    public Boolean verifyMyProfileLabelIsDisplayed() throws Exception {
+        log.info("Step: Verify if My Profile Label is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("lblMyProfile", ConfigUtilities.Timers.slow));
+        Assert.assertEquals(excel.getTestdata("lblMyProfileText"), action.getText("lblMyProfile"));
+        return action.isDisplayed("lblMyProfile");
+    }
+
+    // For Edit My Profile Status verification
+    @Step("Step: Verify if My Profile Status is Displayed")
+    public Boolean verifyMyProfileStatusIsDisplayed() throws Exception {
+        log.info("Step: Verify if My Profile Status is Displayed");
+        Assert.assertTrue(action.waitForElementToBeVisible("lblMyProfileStatus", ConfigUtilities.Timers.slow));
+        return action.isDisplayed("lblMyProfileStatus");
+    }
+
+    // For Go to My Profile Button verification
+    @Step("Step: Verify if Go to My Profile Button is Enabled")
+    public Boolean verifyBtnGoToMyProfileIsEnabled() throws Exception {
+        log.info("Step: Verify if Go to My Profile Button is Enabled");
+        Assert.assertTrue(action.waitForElementToBeVisible("goToMyProfileBtn", ConfigUtilities.Timers.slow));
+        return action.isDisplayed("goToMyProfileBtn");
+    }
+
+    @Step("Step: Click Go to My Profile")
+    public void clickBtnGoToMyProfile() throws Exception {
+        log.info("Step: Click Go to My Profile");
+        action.takeSnapShot("User clicked Go to My Profile");
+        action.waitForElementToBeClickable("goToMyProfileBtn",ConfigUtilities.Timers.slow);
+        action.click("goToMyProfileBtn");
+    }
+
+
+    // ****************** STEPS ****************************** //
+
+    @Step("Step: Navigate Account Details Page")
+    public void accountDetailsPage() throws Exception {
+        verifyLinkAccountDetailsIsEnabled();
+        clickLinkAccountDetails();
+    }
+
+    @Step("Step: Check for the Account Details Page Content")
+    public void accountDetailsPageContent() throws Exception {
+        verifyNicknameAccountDetailsIsDisplayed();
+        verifyAccountDetailsTxtIsDisplayed();
+        verifyCameraIconIsEnable();
+        verifyAccountImageIsDisplayed();
+        verifyHamburgerBtnIsEnabled();
+        verifyEditNicknameLinkIsEnabled();
+        verifyLblPrepaidNumberIsDisplayed();
+        verifyPrepaidNumberIsDisplayed();
+        verifyPrepaidNumberStatusIsDisplayed();
+        verifyMyProfileLabelIsDisplayed();
+        verifyMyProfileStatusIsDisplayed();
+        verifyBtnGoToMyProfileIsEnabled();
+    }
+
+    @Step("Step: Check for the Account Details Page Content")
+    public void clickGoToMyProfile() throws Exception {
+        accountDetailsPage();
+        accountDetailsPageContent();
+        clickBtnGoToMyProfile();
+    }
+
+    @Step("Step: Check for the Account Details Page Content")
+    public void editNickName() throws Exception {
+        accountDetailsPage();
+        accountDetailsPageContent();
+        clickEditNickname();
+    }
+
+}
---
+++ src/test/java/testcases/VerifyAccountDetailsPage.java
@@ -0,0 +1,61 @@
+package testcases;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(testcases.ListenerTestcase.class)
+public class VerifyAccountDetailsPage extends BaseTestcase{
+
+    @Test(testName = "VerifyAccountDetailsPage", priority = 1)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: User will check for the Account Details Page")
+    @Story("Story: Account Details Page")
+    public void verifyAccountDetailsPage() throws Exception {
+        getStartedPage.verifyIfGetStartedPage();
+        getStartedPage.clickBtnGetStarted();
+        welcomePage.verifyIfLoginPage();
+        welcomePage.enterPrepaidNumber(excel.getTestdata("mobileNumber"));
+        welcomePage.verifyIfBtnNextIsEnabled();
+        welcomePage.clickBtnNext();
+        secureAppPage.verifyIfSecurePage();
+        secureAppPage.enterPin(excel.getTestdata("pin"));
+        secureAppPage.clickBtnNext();
+        secureAppPage.verifyIfSecurePageConfirmation();
+        secureAppPage.enterPin(excel.getTestdata("pin"));
+        secureAppPage.clickBtnNext();
+        customerProfilePage.verifyIfCustomerProfilePage();
+        customerProfilePage.enterNickname(excel.getTestdata("nickname"));
+        customerProfilePage.verifyIfBtnNextIsEnabled();
+        customerProfilePage.clickBtnNext();
+        chooseModemPage.verifyChooseModemFour();
+        modemConfigPage.verifyUserAlreadyConnected();
+        homePage.navigateToHomePage();
+        homePage.clickHamburgerBtn();
+        accountDetailsPage.accountDetailsPage();
+        accountDetailsPage.accountDetailsPageContent();
+    }
+
+
+    @Test(testName = "Verify Go to My Profile page", priority = 2)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: User will click and go to My Profile Page")
+    @Story("Story: Go to My Profile")
+    public void verifyGotoMyProfilePage() throws Exception {
+        verifyAccountDetailsPage();
+        accountDetailsPage.clickGoToMyProfile();
+    }
+
+    @Test(testName = "Verify Edit Nickname", priority = 2)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: User will edit nickname")
+    @Story("Story: Edit Nickname")
+    public void verifyEditNickname() throws Exception {
+        verifyAccountDetailsPage();
+        accountDetailsPage.clickEditNickname();
+    }
+
+}
