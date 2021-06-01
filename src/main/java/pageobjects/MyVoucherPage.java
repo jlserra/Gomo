db3e09e6cafcdc316e5433acd8e8155b9b1c69c5
marksaniano <msaniano@stratpoint.com>
2021-06-01 08:41:41 +0800

    Verify My Voucher page

--- src/main/java/pageobjects/MyVoucherPage.java
+++ src/main/java/pageobjects/MyVoucherPage.java
@@ -1,6 +1,12 @@
 package pageobjects;
 
 import io.appium.java_client.MobileDriver;
+import io.qameta.allure.Step;
+import org.testng.Assert;
+import utilities.ConfigUtilities;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class MyVoucherPage extends BasePage{
 
@@ -8,6 +14,31 @@ public class MyVoucherPage extends BasePage{
 
     public MyVoucherPage(MobileDriver driver) {
         this.driver = driver;
+    }
+
+    @Step("Step: Click My Voucher Button")
+    public void clickMyVoucherBtn () throws Exception {
+        log.info("Step: Click My Voucher Button");
+        action.takeSnapShot("User clicked the My Voucher Link");
+        action.waitForElementToBeClickable("lnkMyVouchers",ConfigUtilities.Timers.slow);
+        action.click("lnkMyVouchers");
+    }
+
+    @Step("Step: Verify if user is in My Voucher Page")
+    public void verifyIfMyVoucherPage() throws Exception {
+
+        log.info("Step: Verify My Voucher Page");
+        assertEquals(excel.getTestdata("text1"), action.getText("txtMyVouchersTitle"));
+        action.takeSnapShot("User is in My Voucher Page");
+    }
+
+    @Step("Step: Verify Voucher")
+    public void verifyVoucher() throws Exception {
+
+        log.info("Step: Verify Voucher");
+        assertTrue(action.waitForElementToBeVisible("txtNoVoucher", ConfigUtilities.Timers.slow));
+        assertEquals(excel.getTestdata("text2"), action.getText("txtNoVoucher"));
+        action.takeSnapShot("User has no Voucher");
     }
 
 
--- src/test/java/testcases/VerifyMyVoucherPage.java
+++ src/test/java/testcases/VerifyMyVoucherPage.java
@@ -7,8 +7,63 @@ import io.qameta.allure.Story;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 @Listeners(testcases.ListenerTestcase.class)
 public class VerifyMyVoucherPage extends BaseTestcase{
 
+    @Test(testName = "VerifyMyVoucherPage", priority = 1)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: User will be redirected to My Voucher Page")
+    @Story("Story: My Voucher Page")
+    public void navigateToMyVoucherPage() throws Exception {
+
+        //Get Started
+        assertTrue(getStartedPage.verifyIfGetStartedPage());
+        getStartedPage.clickBtnGetStarted();
+
+        //Welcome Page
+        assertTrue(welcomePage.verifyIfLoginPage());
+        welcomePage.enterPrepaidNumber(excel.getTestdata("mobileNumber"));
+        assertTrue(welcomePage.verifyIfBtnNextIsEnabled());
+        welcomePage.clickBtnNext();
+
+        //Secure Page
+        assertTrue(secureAppPage.verifyIfSecurePage());
+        secureAppPage.enterPin(excel.getTestdata("pin"));
+        secureAppPage.clickBtnNext();
+        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
+        secureAppPage.enterPin(excel.getTestdata("pin"));
+        secureAppPage.clickBtnNext();
+
+        //Customer Profile
+        assertTrue(customerProfilePage.verifyIfCustomerProfilePage());
+        customerProfilePage.enterNickname(excel.getTestdata("nickname"));
+        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
+        customerProfilePage.clickBtnNext();
+
+        //Choose Modem
+        chooseModemPage.verifyChooseModemFour();
+        modemConfigPage.verifyUserAlreadyConnected();
+
+        //Home Page
+        homePage.clickBtnProceed();
+        homePage.clickHamburgerBtn();
+
+        //Click My Voucher link
+        myVoucherPage.clickMyVoucherBtn();
+        //My Voucher Page
+        myVoucherPage.verifyIfMyVoucherPage();
+
+    }
+
+    @Test(testName = "VerifyMyVoucherPage", priority = 2)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Test Description: User will check the Voucher")
+    @Story("Story: My Voucher Page")
+    public void verifyMyVoucher() throws Exception{
+        navigateToMyVoucherPage();
+        myVoucherPage.verifyVoucher();
+    }
 
 }
