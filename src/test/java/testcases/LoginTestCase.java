833aba1f16396bf4889b18882cb47e739d455996
marksaniano <msaniano@stratpoint.com>
2021-06-01 18:24:40 +0800

    login

--- src/test/java/testcases/LoginTestCase.java
+++ src/test/java/testcases/LoginTestCase.java
@@ -4,18 +4,20 @@ import io.qameta.allure.*;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 @Listeners(testcases.ListenerTestcase.class)
 public class LoginTestCase extends BaseTestcase {
 
-//    @Test(testName = "loginUser")
+    @Test(testName = "loginUser")
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Description: User will be logged in to the system")
     @Story("Story: Login")
     public void loginUser() throws Exception {
 
-        String mobileNumber = "09271080510";
-        String pin = "1111";
-        String nickname = "test nickname";
+//        String mobileNumber = "09271080510";
+//        String pin = "1111";
+//        String nickname = "test nickname";
 
 
 //        Get Started Page
@@ -60,6 +62,37 @@ public class LoginTestCase extends BaseTestcase {
 
 //        action.implicitlyWait();
 //        action.takeSnapShot("Dashboard Screen");
+
+        //Get Started
+        assertTrue(getStartedPage.verifyIfGetStartedPage());
+        getStartedPage.clickBtnGetStarted();
+
+        //Welcome Page
+        assertTrue(welcomePage.verifyIfLoginPage());
+        welcomePage.enterPrepaidNumber(excel.getTestdata("mobileNumber"));
+        assertTrue(welcomePage.verifyIfBtnNextIsEnabled());
+        welcomePage.clickBtnNext();
+
+        //Secure Page
+        assertTrue(secureAppPage.verifyIfSecurePage());
+        secureAppPage.enterPin(excel.getTestdata("pin"));
+        secureAppPage.clickBtnNext();
+        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
+        secureAppPage.enterPin(excel.getTestdata("pin"));
+        secureAppPage.clickBtnNext();
+
+        //Customer Profile
+        assertTrue(customerProfilePage.verifyIfCustomerProfilePage());
+        customerProfilePage.enterNickname(excel.getTestdata("nickname"));
+        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
+        customerProfilePage.clickBtnNext();
+
+        //Choose Modem
+        chooseModemPage.verifyChooseModemFour();
+        modemConfigPage.verifyUserAlreadyConnected();
+
+        //Home Page
+        homePage.clickBtnProceed();
 
     }
 
--- src/test/java/testcases/VerifyMyVoucherPage.java
+++ src/test/java/testcases/VerifyMyVoucherPage.java
@@ -11,48 +11,15 @@ import static org.testng.Assert.assertTrue;
 
 @Listeners(testcases.ListenerTestcase.class)
 public class VerifyMyVoucherPage extends BaseTestcase{
-
+//      LoginTestCase login;
+//    LoginTestCase login = new LoginTestCase();
     @Test(testName = "VerifyMyVoucherPage", priority = 1)
     @Severity(SeverityLevel.NORMAL)
     @Description("Test Description: User will be redirected to My Voucher Page")
     @Story("Story: My Voucher Page")
     public void navigateToMyVoucherPage() throws Exception {
-
-        //Get Started
-        assertTrue(getStartedPage.verifyIfGetStartedPage());
-        getStartedPage.clickBtnGetStarted();
-
-        //Welcome Page
-        assertTrue(welcomePage.verifyIfLoginPage());
-        welcomePage.enterPrepaidNumber(excel.getTestdata("mobileNumber"));
-        assertTrue(welcomePage.verifyIfBtnNextIsEnabled());
-        welcomePage.clickBtnNext();
-
-        //Secure Page
-        assertTrue(secureAppPage.verifyIfSecurePage());
-        secureAppPage.enterPin(excel.getTestdata("pin"));
-        secureAppPage.clickBtnNext();
-        assertTrue(secureAppPage.verifyIfSecurePageConfirmation());
-        secureAppPage.enterPin(excel.getTestdata("pin"));
-        secureAppPage.clickBtnNext();
-
-        //Customer Profile
-        assertTrue(customerProfilePage.verifyIfCustomerProfilePage());
-        customerProfilePage.enterNickname(excel.getTestdata("nickname"));
-        assertTrue(customerProfilePage.verifyIfBtnNextIsEnabled());
-        customerProfilePage.clickBtnNext();
-
-        //Choose Modem
-        chooseModemPage.verifyChooseModemFour();
-        modemConfigPage.verifyUserAlreadyConnected();
-
-        //Home Page
-        homePage.clickBtnProceed();
         homePage.clickHamburgerBtn();
-
-        //Click My Voucher link
         myVoucherPage.clickMyVoucherBtn();
-        //My Voucher Page
         myVoucherPage.verifyIfMyVoucherPage();
 
     }
@@ -62,7 +29,8 @@ public class VerifyMyVoucherPage extends BaseTestcase{
     @Description("Test Description: User will check the Voucher")
     @Story("Story: My Voucher Page")
     public void verifyMyVoucher() throws Exception{
-        navigateToMyVoucherPage();
+        homePage.clickHamburgerBtn();
+        myVoucherPage.clickMyVoucherBtn();
         myVoucherPage.verifyVoucher();
     }
 
