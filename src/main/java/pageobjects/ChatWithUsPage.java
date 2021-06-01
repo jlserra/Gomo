98dee87589e5707aad459d34d312d15ab8f4f95e
marksaniano <msaniano@stratpoint.com>
2021-06-01 16:56:25 +0800

    Chat with us page

---
+++ src/main/java/pageobjects/ChatWithUsPage.java
@@ -0,0 +1,11 @@
+package pageobjects;
+
+import io.appium.java_client.MobileDriver;
+
+public class ChatWithUsPage extends BasePage{
+    MobileDriver driver;
+
+    public ChatWithUsPage(MobileDriver driver) {
+        this.driver = driver;
+    }
+}
---
+++ src/test/java/testcases/VerifyChatWithUsPage.java
@@ -0,0 +1,6 @@
+package testcases;
+
+public class VerifyChatWithUsPage extends BaseTestcase{
+
+
+}
